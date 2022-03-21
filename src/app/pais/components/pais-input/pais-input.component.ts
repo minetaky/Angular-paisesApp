4422b9caa7a942d8103fb28dfddddcfea58595bf
Veronica M. Melendez Montiel <minetaky@gmail.com>
2022-03-21 17:55:15 -0600

    Debounce en input buscar

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,4 +1,5 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-pais-input',
@@ -7,14 +8,30 @@ import { Component, Output, EventEmitter } from '@angular/core';
   ]
 })
 
-export class PaisInputComponent {
-
+export class PaisInputComponent implements OnInit {
 @Output() onEnter: EventEmitter<string> = new EventEmitter();
+@Output() onDebounce: EventEmitter<string> = new EventEmitter();
+
+debouncer: Subject<string> = new Subject();
 
  termino: string = '';
+
+ ngOnInit(): void {
+ this.debouncer
+ .pipe(debounceTime(300))
+ .subscribe(valor => {
+   this.onDebounce.emit( valor );
+ });
+}
 
  buscar (){
    this.onEnter.emit( this.termino );
  }
+
+ teclaPresionada(){
+
+  this.debouncer.next( this.termino );
+
+}
 
 }
