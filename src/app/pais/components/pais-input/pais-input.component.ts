1d60311b1ece891894aa17e598a909d09ed1daf0
Veronica M. Melendez Montiel <minetaky@gmail.com>
2022-03-20 21:33:45 -0600

    componente tabla e input

---
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pais-input',
+  templateUrl: './pais-input.component.html',
+  styles: [
+  ]
+})
+export class PaisInputComponent {
+
+ termino: string = '';
+
+ buscar (){
+   console.log(this.termino);
+ }
+
+}
